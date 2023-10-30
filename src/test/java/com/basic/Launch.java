5b46c7c72da8a379518b90688355aef96f318396
ShubhamShankarJagtap <shubh.jagtap14 @gmail.com>
2023-10-30 23:11:55 +0530

    This is commit after ading broken link sorce code.

--- src/test/java/com/basic/Launch.java
+++ src/test/java/com/basic/Launch.java
@@ -2,12 +2,18 @@ package com.basic;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -19,7 +25,7 @@ public class Launch {
 
     WebDriver driver;
 
-    @Test
+    @BeforeClass
     public void launchBrowser() {
 
         WebDriverManager.chromedriver().setup();
@@ -29,7 +35,6 @@ public class Launch {
     }
 
     @Test
-
     public void screenShoot() throws IOException {
 
         TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
@@ -37,5 +42,40 @@ public class Launch {
         File sourcFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(sourcFile, destFile);
 
+    }
+
+    @Test
+    public void brokenLinks() {
+
+        driver.navigate().to("https://www.redbus.in");
+
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        for (WebElement link : links) {
+
+            String href = link.getAttribute("href");
+
+            if (href != null && !href.isEmpty()) {
+                try {
+
+                    URL url = new URL(href);
+
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.connect();
+
+                    int responseCode = urlConnection.getResponseCode();
+
+                    if (responseCode >= 400) {
+
+                        System.out.println("Broken link is : " + href + "responseCode is : " + responseCode);
+                    } else {
+
+                        System.out.println("Valid link is : " + href);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(" Error connecting to : " + href);
+                }
+            }
+        }
     }
 }
