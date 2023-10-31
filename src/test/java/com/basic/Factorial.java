ba79bab920ddf48d6793e949f21655e0ce745ff5
ShubhamShankarJagtap <shubh.jagtap14 @gmail.com>
2023-10-31 23:28:33 +0530

    New Commit

---
+++ src/test/java/com/basic/Factorial.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+public class Factorial {
+
+   static private int factorial(int number){
+
+    
+    int fact=1;
+
+    for(int i=1;i<=number;i++){
+        
+        fact = number*i; 
+    }
+    
+
+    return fact;
+
+        lic static void main(Stri
+
+        int Fact = factorial(22);
+
+       System.out.println("Factorial of given number is : " + Fact);
+    }
+}
